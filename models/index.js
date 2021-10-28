04d4307d1ecd92373468b119d6c72600d82aa8d9
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-10-27 23:41:58 -0700

    votes started

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require("./Vote");
 
 //create association User > Post
 User.hasMany(Post, {
@@ -9,6 +10,18 @@ User.hasMany(Post, {
 //reverse association Post > User
 Post.belongsTo(User, {
   foreignKey: "user_id",
+});
+
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
 });
 
 module.exports = { User, Post };
