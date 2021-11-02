da54a74d6110bfc5cebcadaddcd0e28440306127
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-01 23:24:47 -0700

    created file, connected files, and coded log out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
   console.log("======================");
   Post.findAll({
     attributes: [
@@ -44,6 +45,11 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
   res.render("login");
 });
 
